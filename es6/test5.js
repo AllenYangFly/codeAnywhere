7084530afa063b8bbfb9b0857b44f3aeef6e3a73
yangyazhou <yangyazhou@youzan.com>
2017-06-28 16:55:30 +0800

    fix: 添加promise

--- es6/test5.js
+++ es6/test5.js
@@ -1,8 +1,39 @@
 let node = {
     type: 'Identifier',
-    name: 'foo'
+    name: 'foo',
+    loc: {
+        price: 10
+    }
 }
 
-let {type, name} = node;
+// 对象解构
+// let {type, name} = node;
+
+// 对象嵌套解构
+let {type, name, loc:{ price }} = node
 
 console.log(type);
+console.log(price);
+
+
+
+// 数组解构
+let [fristColor, ...secondColor] = ['red', 'bule', 'white'];
+console.log(fristColor);
+console.log(secondColor);
+
+// get & set
+var test = {
+    get age () {
+        return age+'*';
+    },
+    set age (value) {
+        age = value+1;
+    }
+}
+
+test.age = 1;
+console.log(test.age);
+
+
+// static
