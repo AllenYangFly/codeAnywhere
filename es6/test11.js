baa21ac677c4e244df825b71d6e11ac2a80d0391
yangyazhou <yangyazhou@youzan.com>
2017-06-28 17:02:29 +0800

    fix: 添加promise

--- es6/test11.js
+++ es6/test11.js
@@ -2,5 +2,14 @@
  * pormise 
  * then() 两个参数，第一个参数是resolve，第二个参数为reject
  * catch() 只传递reject的handler
-
  */
+
+var promise = new Pormie((val) => {
+    if(val > 5) {
+        resolve(val);
+    } else {
+        reject(val)
+    }
+})
+
+promise.then()
